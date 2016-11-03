ce7ff9c927bf0742e5aab4089119ecea4d1e11ba
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-11-03 16:51:58 -0400

    created campaign and candidate classes and test files

---
+++ server/test/campaign_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative 'campaign'
+require_relative 'candidate'
+
+class CampaignTest < Minitest::Test
+end
---
+++ server/test/candidate_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative 'campaign'
+require_relative 'candidate'
+
+class CandidateTest < Minitest::Test
+
+end
