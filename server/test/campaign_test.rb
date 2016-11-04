2ad6935e4099d4bc7f71fbb4ee17c6b754ed5fa1
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-11-04 11:06:43 -0400

    tested creating and updating campaigns and candidates in our db

--- server/test/campaign_test.rb
+++ server/test/campaign_test.rb
@@ -1,8 +1,29 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-
-require_relative 'campaign'
-require_relative 'candidate'
+require_relative '../dependencies'
 
 class CampaignTest < Minitest::Test
+  def setup
+    Campaign.delete_all
+    Candidate.delete_all
+  end
+
+  def test_class_exists
+    assert Campaign
+  end
+
+  def test_can_create_candidate
+    now = Date.today
+    Campaign.create!(start_date: now)
+    assert_equal now, Campaign.last.start_date
+  end
+
+  def test_can_update_candidate
+    now = Date.today
+    new_date = Date.new(2015, 1, 1)
+    first_campaign = Campaign.create!(start_date: now)
+    first_campaign.update(start_date: new_date)
+    first_campaign.save
+    assert_equal new_date, Campaign.find(first_campaign.id).start_date
+  end
 end
--- server/test/candidate_test.rb
+++ server/test/candidate_test.rb
@@ -11,4 +11,16 @@ class CandidateTest < Minitest::Test
   def test_class_exists
     assert Candidate
   end
+
+  def test_can_create_candidate
+    Candidate.create!(name: "Kvothe", image_url: "google.com", intelligence: 5)
+    assert_equal "Kvothe", Candidate.last.name
+  end
+
+  def test_can_update_candidate
+    kvothe = Candidate.create!(name: "Kvothe", image_url: "google.com", intelligence: 5)
+    kvothe.update(name: "Kvothe Kingkiller")
+    kvothe.save
+    assert_equal "Kvothe Kingkiller", Candidate.find(kvothe.id).name
+  end
 end
